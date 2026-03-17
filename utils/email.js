a3e7af3826fa855e459f97fcf9bf8a6863d373d9
SWAYAMPATEL30 <patelswayam400@gmail.com>
2026-03-17 06:24:55 +0530

    fix: gmail smtp connection timeout on render

--- utils/email.js
+++ utils/email.js
@@ -2,7 +2,9 @@ import nodemailer from 'nodemailer';
 
 // configure once using environment variables
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // IMPORTANT for cloud platforms
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD
